84e659d1527d837e81a9f69f3ef9e75baef09dba
Adedeji Yusuf <adedejiyusuf26@gmail.com>
2021-02-22 16:12:37 +0100

    After Calculator

---
+++ simple_calculator/simple_calculator.php
@@ -0,0 +1,55 @@
+<?php 
+extract($_POST);
+if(isset($operator))
+{
+	switch($operator)
+	{
+		case 'Add':
+		$result = $first_number + $second_number;
+		break;
+		
+		case 'Subtract':
+		$result = $first_number - $second_number;
+		break;
+		
+		case 'Multiply':
+		$result = $first_number * $second_number;
+		break;
+
+		case 'Divide':
+		$result = $first_number / $second_number;
+		break;
+		
+	}
+	
+}
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Side-Hustle - Calculator</title>
+		
+	</head>
+	<body>
+
+        <h3>SIDE HUSTLE - SIMPLE CALCULATOR</h3>
+
+		<form method="post">
+		    <p>Your result</p>
+            <input type="number" disabled="disabled" value="<?php  echo @$result;?>"/>
+			
+			<p>Enter your First Number</p>
+            <input type="number" name="first_number" value="<?php  echo @$first_number;?>"/> <br><br>
+			
+			<p>Enter your Second Number</p>
+            <input type="number" name="second_number" value="<?php  echo @$second_number;?>"/> <br><br>
+			
+			<input type="submit" name="operator" value="Add" />
+            <input type="submit" name="operator" value="Subtract" />
+            <input type="submit" name="operator" value="Multiply" />
+            <input type="submit" name="operator" value="Divide" />
+
+		</form>
+	</body>
+</html>
